ddddbbf4e0bd10ae679583a70d0efd9cad2e67f9
Pat Downey <pat.downey@gmail.com>
2017-09-06 23:26:07 +0100

    fixup build

--- build.sh
+++ build.sh
@@ -1,28 +1,54 @@
 #!/bin/bash
+set -xe
 
-MAINTAINER="pat.downey@gmail.com"
-URL="https://github.com/patdowney/secure-demo-hiera"
-PACKAGE_NAME="secure-demo-hiera"
+PACKAGE_MAINTAINER="pat.downey@gmail.com"
+PACKAGE_URL="https://github.com/patdowney/secure-demo-hiera"
 
 bundle install --path vendor/bundle --binstubs .bundle/bin
 
+bundle exec librarian-puppet package --strip-dot-git --path=vendor/modules --verbose
+bundle exec librarian-puppet install --local --strip-dot-git --path=vendor/modules --verbose
 
-find ./data -name '*.yaml' -exec ruby -e  "require 'yaml'; YAML.parse_file(\"{}\");" \;
+bundle exec puppet-lint --no-80chars-check --no-140chars-check --fail-on-warnings --with-context --with-filename modules
+bundle exec puppet-lint --no-80chars-check --no-140chars-check --fail-on-warnings --with-context --with-filename manifests
 
 VERSION=${TRAVIS_BUILD_NUMBER:-0}
+
+PACKAGE_NAME=${PACKAGE_NAME:-$(basename `pwd`)}
 
 mkdir -p .output
 rm -rf .output/*
 
-bundle exec  fpm -t deb -s dir \
-  --package ".output/${PACKAGE_NAME}_0.1.${VERSION}_all.deb" \
+PACKAGE_TYPE="deb"
+bundle exec  fpm -t ${PACKAGE_TYPE} -s dir \
+  --prefix /usr/share/${PACKAGE_NAME} \
+  --package ".output/${PACKAGE_NAME}_${PACKAGE_VERSION}-${VERSION}_all.${PACKAGE_TYPE}" \
   --name "${PACKAGE_NAME}" \
-  --version "${VERSION}" \
-  --depends 'puppet' \
+  --version "${PACKAGE_VERSION}-${VERSION}" \
   --architecture all \
-  --maintainer ${MAINTAINER} \
-  --url ${URL} \
+  --maintainer "$PACKAGE_MAINTAINER" \
+  --url $PACKAGE_URL \
   --deb-user root \
   --deb-group root \
-  hiera.yaml=/etc/hiera/hiera.yaml \
-  data=/etc/hiera/
+  --verbose \
+  bin/ \
+  manifests/ \
+  $(find modules/ -name files -or -name lib -or -name manifests -or -name templates) \
+  $(find vendor/modules/ -name files -or -name lib -or -name manifests -or -name templates)
+
+PACKAGE_TYPE="rpm"
+bundle exec  fpm -t ${PACKAGE_TYPE} -s dir \
+  --prefix /usr/share/${PACKAGE_NAME} \
+  --package ".output/${PACKAGE_NAME}_${PACKAGE_VERSION}-${VERSION}_all.${PACKAGE_TYPE}" \
+  --name "${PACKAGE_NAME}" \
+  --version "${PACKAGE_VERSION}-${VERSION}" \
+  --architecture all \
+  --maintainer "$PACKAGE_MAINTAINER" \
+  --url $PACKAGE_URL \
+  --rpm-user root \
+  --rpm-group root \
+  --verbose \
+  bin/ \
+  manifests/ \
+  $(find modules/ -name files -or -name lib -or -name manifests -or -name templates) \
+  $(find vendor/modules/ -name files -or -name lib -or -name manifests -or -name templates)
